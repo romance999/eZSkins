86724922ceadba7f5d64b5641e63a3cb894cba54
Woke <66000596+woke999@users.noreply.github.com>
2021-12-30 18:47:41 -0800

    Add files via upload

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,10 +1,23 @@
-const SteamUser = require('steam-user');
-const colors = require('colors');
-const client = new SteamUser();
 const login = require("./json/account.json");
-const Options = {
+
+const loginhook = require("./json/loginwebhook.json");
+
+const SteamUser = require('steam-user');
+
+const SteamTotp = require('steam-totp');
+
+const client = new SteamUser();
+
+const colors = require('colors');
+
+const axios = require("axios").default;
+
+
+const Options =
+{
     accountName: login.accountName,
-    password: login.password
+    password: login.password,
+    //twoFactorCode: SteamTotp.generateAuthCode(login.secret) 
 }
 
 console.log("Attempting login".red);
@@ -14,5 +27,22 @@ client.on('loggedOn', () =>
 {
     console.log('');
     console.log('Successfully logged in as '.green + login.accountName.green);
-    client.setPersona(SteamUser.EPersonaState.Snooze); //set the state
+    sendWebhook(loginhook.url, "Successfully logged in as" + login.accountName);
+    client.setPersona(SteamUser.EPersonaState.Busy); //set the state
 });
+
+function sendWebhook(url, content)
+{
+    let msg = {
+        "content": content
+    };
+    
+    axios({
+      url: url,
+      data: JSON.stringify(msg),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+}
