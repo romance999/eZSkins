3ea796ceb4a0e6cc93f1e4aa1572fb4ae179d864
Woke <66000596+woke999@users.noreply.github.com>
2022-01-06 18:06:38 -0500

    Add clear console

---
+++ Bot/Handler/Env.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+function cls()
+{
+    console.clear();
+}
+
+module.exports =
+{
+    cls
+}
--- Bot/Handler/KeyPress.js
+++ Bot/Handler/KeyPress.js
@@ -2,6 +2,9 @@ const keypress = require('keypress');
 const Steam = require("./Steam.js");
 const Webhook = require("./Webhook.js");
 const loginhook = require("../json/loginwebhook.json");
+const login = require("../json/account.json");
+const env = require('./Env.js');
+
 
 
      // listen for the "keypress" event
@@ -28,6 +31,19 @@ const loginhook = require("../json/loginwebhook.json");
          if (key.name == 'e') 
          {
             process.exit(1)
+         }
+     });
+     process.stdin.on('keypress', function (ch, key) 
+     {
+         if (key.name == 'r') 
+         {
+            process.stdin.pause();
+            env.cls();
+            console.log("Logged in as " + login.accountName)
+            console.log("");
+            console.log("Hotkeys: ");
+            console.log("q: Logout | w: Relog | e: exit | r: clear console");
+            process.stdin.resume();
          }
      });
 
