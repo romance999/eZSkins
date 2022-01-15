f6ecbac11eb6d6a9e79f0525452d6eff509ea11b
Woke <66000596+woke999@users.noreply.github.com>
2022-01-15 17:53:44 -0500

    Add files via upload

--- Bot/Handler/RandomGen.js
+++ Bot/Handler/RandomGen.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 
 const colors = require('colors');
 
+const webLink = require("../json/webhook.json");
+
+const Webhook = require("./Webhook.js");
+
 var jackpot;
 
 
@@ -11,6 +15,7 @@ function generateJackpot(max)
     jackpot = Math.floor(Math.random() * max);
     console.log("");
     console.log('Jackpot number generated: '.gray + jackpot + ' out of '.gray + max);
+    Webhook.SendWebhook(webLink.jackpot, 'Jackpot number generated: ' + jackpot + ' out of ' + max);
 }
 
 module.exports =
